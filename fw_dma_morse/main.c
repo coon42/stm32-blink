da2e5ce4a26660c01aa77328ee28e54c8ecdabf2
Matthias Hannig <matthias@hannig.cc>
2017-12-05 18:17:06 +0100

    moved to TIM3

--- fw_dma_morse/main.c
+++ fw_dma_morse/main.c
@@ -18,6 +18,7 @@
  * Now with morse encoding
  *
  * Board: Standard aliexpress stm32f103c8t6
+ * Update: now using TIM3
  */
 
 #define GPIO_LED_PORT GPIOC
@@ -76,56 +77,56 @@ void init_dma()
     rcc_periph_clock_enable(RCC_DMA1);
 
     // Setup NVIC
-    nvic_enable_irq(NVIC_DMA1_CHANNEL2_IRQ);
+    nvic_enable_irq(NVIC_DMA1_CHANNEL3_IRQ);
 
     // Setup DMA1 controller:
     // We are using channel 2 for transfer (TIM2_UP)
     // Transfer size is 16 bit
-    dma_set_memory_size(DMA1, DMA_CHANNEL2, DMA_CCR_MSIZE_16BIT);
-    dma_set_peripheral_size(DMA1, DMA_CHANNEL2, DMA_CCR_PSIZE_16BIT);
+    dma_set_memory_size(DMA1, DMA_CHANNEL3, DMA_CCR_MSIZE_16BIT);
+    dma_set_peripheral_size(DMA1, DMA_CHANNEL3, DMA_CCR_PSIZE_16BIT);
 
     // Transfer mode: Mem to Periph
-    dma_set_read_from_memory(DMA1, DMA_CHANNEL2);
+    dma_set_read_from_memory(DMA1, DMA_CHANNEL3);
 
     // Source and Destination
-    dma_set_memory_address(DMA1, DMA_CHANNEL2,     (uint32_t)&msg_buf);
-    dma_set_peripheral_address(DMA1, DMA_CHANNEL2, (uint32_t)&TIM2_CCR1);
+    dma_set_memory_address(DMA1, DMA_CHANNEL3,     (uint32_t)&msg_buf);
+    dma_set_peripheral_address(DMA1, DMA_CHANNEL3, (uint32_t)&TIM3_CCR1);
 
     // Length
-    dma_set_number_of_data(DMA1, DMA_CHANNEL2, MSG_LEN);
+    dma_set_number_of_data(DMA1, DMA_CHANNEL3, MSG_LEN);
 
     // Repeat and increment pointer with each request
-    dma_enable_circular_mode(DMA1, DMA_CHANNEL2);
-    dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL2);
+    dma_enable_circular_mode(DMA1, DMA_CHANNEL3);
+    dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL3);
 
     // Enable interrupts:
     //  - Interrupt after transfer complete
-    dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL2);
+    dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL3);
 
     // Enable channel
-    dma_enable_channel(DMA1, DMA_CHANNEL2);
+    dma_enable_channel(DMA1, DMA_CHANNEL3);
 }
 
 
-void dma1_channel2_isr()
+void dma1_channel3_isr()
 {
     // Transfer complete interrupt flag
-    if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL2, DMA_TCIF)) {
-        dma_clear_interrupt_flags(DMA1, DMA_CHANNEL2, DMA_TCIF);
+    if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL3, DMA_TCIF)) {
+        dma_clear_interrupt_flags(DMA1, DMA_CHANNEL3, DMA_TCIF);
     }
 }
 
 
 void init_blink()
 {
-    rcc_periph_clock_enable(RCC_TIM2);
+    rcc_periph_clock_enable(RCC_TIM3);
 
     // Setup NVIC
-    nvic_enable_irq(NVIC_TIM2_IRQ);
-    nvic_set_priority(NVIC_TIM2_IRQ, 1);
+    nvic_enable_irq(NVIC_TIM3_IRQ);
+    nvic_set_priority(NVIC_TIM3_IRQ, 1);
 
     // Set mode: No Div, Edge aligned, Up
-    TIM2_CR1 |= TIM_CR1_CKD_CK_INT | TIM_CR1_CMS_EDGE | TIM_CR1_DIR_UP;
+    TIM3_CR1 |= TIM_CR1_CKD_CK_INT | TIM_CR1_CMS_EDGE | TIM_CR1_DIR_UP;
 
     // DMA request on update event
     // TIM2_CR2 |= TIM_CR2_CCDS;
@@ -134,31 +135,31 @@ void init_blink()
     //  - Compare Channel 1 (CC1IE)
     //  - Update / Overflow Event (UIE)
     //  - Update DMA Request Enable (UDE)
-    TIM2_DIER |= TIM_DIER_UIE | TIM_DIER_CC1IE | TIM_DIER_UDE;
+    TIM3_DIER |= TIM_DIER_UIE | TIM_DIER_CC1IE | TIM_DIER_UDE;
 
     // Configure prescaler
-    TIM2_ARR = 65514; // Full range
-    TIM2_PSC = 549; // 0.5s
+    TIM3_ARR = 65514; // Full range
+    TIM3_PSC = 549; // 0.5s
 
     // Set compare value
-    TIM2_CCR1 = 0;
+    TIM3_CCR1 = 0;
 
     // Enable timer
-    TIM2_CR1 |= TIM_CR1_CEN;
+    TIM3_CR1 |= TIM_CR1_CEN;
 }
 
 
-void tim2_isr()
+void tim3_isr()
 {
-    if(TIM2_SR & TIM_SR_UIF) {
+    if(TIM3_SR & TIM_SR_UIF) {
         gpio_clear(GPIO_LED_PORT, GPIO_LED_PIN);
-        TIM2_SR &= ~TIM_SR_UIF; // Clear flag
+        TIM3_SR &= ~TIM_SR_UIF; // Clear flag
     }
 
-    if(TIM2_SR & TIM_SR_CC1IF) {
+    if(TIM3_SR & TIM_SR_CC1IF) {
         // Compare flag triggered, pulse off
         gpio_set(GPIO_LED_PORT, GPIO_LED_PIN);
-        TIM2_SR &= ~TIM_SR_CC1IF; // Clear flag
+        TIM3_SR &= ~TIM_SR_CC1IF; // Clear flag
     }
 }
 
