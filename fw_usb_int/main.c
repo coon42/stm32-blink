84afdf2c61ee1c802b809a7105d46eae6e46278f
Matthias Hannig <matthias@hannig.cc>
2018-02-23 21:14:14 +0100

    added improved usb hhandling firmware

---
+++ fw_usb_int/main.c
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2010 Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#include "usb_serial.h"
+
+/*
+ * Override _write and redirect stdout to usb
+ */
+int _write(int file, char* data, int len)
+{
+    if (file < 2) {
+        return usb_serial_tx(data, len);
+    }
+
+    // Set error and return failure
+    errno = EIO;
+    return -1;
+}
+
+
+int main(void)
+{
+	int i;
+    const char* line;
+
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	usbd_device *usbd_dev = usb_serial_init();
+
+	while (1) {
+		// usbd_poll(usbd_dev);
+
+        /*
+        line = usb_serial_rx();
+        if (line) {
+            usb_serial_tx(usbd_dev, "Received a line: ");
+            usb_serial_tx(usbd_dev, line);
+            usb_serial_tx(usbd_dev, "\r\n");
+        }
+        */
+        if( i % 10000  == 0 ) {
+            // usb_serial_tx("fnord42\r\n");
+            printf("Fnord 42 :: %d\r\n", i);
+        }
+
+        i++;
+    }
+}
