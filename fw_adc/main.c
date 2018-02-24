cc246300483a903b70195d2c636de370102dfc6b
Matthias Hannig <matthias@hannig.cc>
2018-02-24 16:56:57 +0100

    initial adc project

---
+++ fw_adc/main.c
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2010 Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#include "usb_serial.h"
+
+
+int main(void)
+{
+	int i;
+    const char* line;
+
+    // Clock Setup
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
+    // Initialize USB
+	usb_serial_init();
+
+	while (1) {
+        line = usb_serial_rx();
+        if (line) {
+            printf("Received a line: %s\r\n", line);
+        }
+
+        if( i % 10000  == 0 ) {
+            printf("Fnord 42 :: %d\r\n", i);
+        }
+
+        i++;
+    }
+}
