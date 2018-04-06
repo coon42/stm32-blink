841a5f9e531147c99938ba19d9acad1e360a115f
Matthias Hannig <matthias@hannig.cc>
2018-04-06 12:28:16 +0200

    added fonts from SDK examples

---
+++ fw_oled/fonts.h
@@ -0,0 +1,19 @@
+
+#ifndef FONTS_H
+#define FONTS_H
+
+
+extern const uint8_t c_chFont1206[95][12];
+extern const uint8_t c_chFont1608[95][16];
+extern const uint8_t c_chFont1612[11][32];
+extern const uint8_t c_chFont3216[11][64];
+extern const uint8_t c_chBmp4016[96];
+extern const uint8_t c_chSingal816[16];
+extern const uint8_t c_chMsg816[16];
+extern const uint8_t c_chBluetooth88[8];
+extern const uint8_t c_chBat816[16];
+extern const uint8_t c_chGPRS88[8];
+extern const uint8_t c_chAlarm88[8];
+
+#endif
+
