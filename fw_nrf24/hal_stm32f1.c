6d2a150514f7909890ff2663841b0ef3febf9d00
coon <coon@mailbox.org>
2018-05-09 20:59:42 +0200

    basic HAL should be implemneted now...

--- fw_nrf24/hal_stm32f1.c
+++ fw_nrf24/hal_stm32f1.c
@@ -1,4 +1,6 @@
 #include "nrf24_hal.h"
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
 
 #define CHIP_ENABLE_PIN GPIO_Pin_13
 #define CHIP_SELECT_PIN GPIO_Pin_14
@@ -13,19 +15,19 @@ void nrf24_ceHigh() {
 }
 
 void nrf24_csnLow() {
-  // TODO: implement
+  gpio_clear(GPIOA, GPIO4);
 }
 
 void nrf24_csnHigh() {
-  // TODO: implement
+  gpio_set(GPIOA, GPIO4);
 }
 
-uint8_t nrf24_getCe() {
-  // TODO: implement
+uint8_t nrf24_getCe() { // GPIO get?
+  return 1; // TODO: implement
 }
 
 uint8_t spiXmitByte(uint8_t value) {
-  // TODO: implement
+  spi_send(SPI1, value);
 }
 
 void spiInit() {
